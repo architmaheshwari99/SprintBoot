a7401243cd968a1102001083d890955cf2959110
maheshwari <archit.maheshwari@payufin.com>
2023-04-01 14:58:40 +0530

    adding actuator, security and injecting property values

--- mycoolapp/src/main/java/com/archit99/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/archit99/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.archit99.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String name;
+
+    @Value("${team.name}")
+    private String team;
+
+    @GetMapping("/teamInfo")
+    public String teamInfo(){
+        return "Hello "+ name +" from " + team;
+    }
 
     @GetMapping("/")
     public String hello(){
