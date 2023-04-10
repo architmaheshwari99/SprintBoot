02f9f33b8bedc2d321ca0aedf0e8b3a04ec0b00c
maheshwari <archit.maheshwari@payufin.com>
2023-04-10 20:41:20 +0530

    prototype bean scope

--- springcoredemo/src/main/java/com/architmahes/springcoredemo/common/BaseballCoach.java
+++ springcoredemo/src/main/java/com/architmahes/springcoredemo/common/BaseballCoach.java
@@ -1,10 +1,13 @@
 package com.architmahes.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //spring.main.lazy_initialization=true in application.properties for global scope
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseballCoach implements Coach{
     @Override
     public String getDailyWorkout() {
