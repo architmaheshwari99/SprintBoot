5137538093bcc73997d9a9bbac0dc8af9a86b406
maheshwari <archit.maheshwari@payufin.com>
2023-04-10 19:01:05 +0530

    configuring lazy initialization property

--- springcoredemo/src/main/java/com/architmahes/springcoredemo/common/BaseballCoach.java
+++ springcoredemo/src/main/java/com/architmahes/springcoredemo/common/BaseballCoach.java
@@ -3,6 +3,7 @@ package com.architmahes.springcoredemo.common;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+//spring.main.lazy_initialization=true in application.properties for global scope
 @Component
 @Lazy
 public class BaseballCoach implements Coach{
