947ae8acb25f76f9880f4f859c5913ee0061e6da
maheshwari <archit.maheshwari@payufin.com>
2023-04-17 08:56:44 +0530

    Controller Advice

--- rest/src/main/java/com/architmahes/rest/rest/StudentRestController.java
+++ rest/src/main/java/com/architmahes/rest/rest/StudentRestController.java
@@ -37,25 +37,5 @@ public class StudentRestController {
         return students.get(studentId);
     }
 
-    // Exception handler to handle the exception
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 
 }
