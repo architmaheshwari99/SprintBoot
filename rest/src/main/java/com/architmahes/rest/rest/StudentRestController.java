077732b1757522c9d7136951984acfbaeadb0ce0
maheshwari <archit.maheshwari@payufin.com>
2023-04-17 07:43:39 +0530

    get student by id

--- rest/src/main/java/com/architmahes/rest/rest/StudentRestController.java
+++ rest/src/main/java/com/architmahes/rest/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.architmahes.rest.rest;
 
 import com.architmahes.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+        students.add(new Student("Genjis", "Khan"));
+        students.add(new Student("Abraham", "Lincon"));
+        students.add(new Student("Laxmi", "Bai"));
+    }
 
     @GetMapping("/students")
     public List<Student> getStudents(){
-        List<Student> l1 = new ArrayList<>();
-        l1.add(new Student("Genjis", "Khan"));
-        l1.add(new Student("Abraham", "Lincon"));
-        l1.add(new Student("Laxmi", "Bai"));
-
-        return l1;
+        return students;
     }
 
+    @GetMapping("/students/{studentId}")
+    public Student getStudents(@PathVariable int studentId){
+        return students.get(studentId);
+    }
 }
