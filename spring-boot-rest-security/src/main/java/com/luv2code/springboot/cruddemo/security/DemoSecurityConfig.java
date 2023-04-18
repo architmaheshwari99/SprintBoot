7cad4d8accc749c39616312e1070a97fe65a10d9
maheshwari <archit.maheshwari@payufin.com>
2023-04-18 23:35:52 +0530

    building user inmemory and trying to authenticate

---
+++ spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test")
+                .roles("EMPLOYEE")
+                .build();
+
+
+        UserDetails casper = User.builder()
+                .username("casper")
+                .password("{noop}test")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+
+        UserDetails archit = User.builder()
+                .username("archit")
+                .password("{noop}test")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, casper, archit);
+
+    }
+}
