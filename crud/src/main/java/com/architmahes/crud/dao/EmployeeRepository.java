5329fbe9a59228718a5bd5dd38364f07723c9368
maheshwari <archit.maheshwari@payufin.com>
2023-04-18 12:26:47 +0530

    Pagination

--- crud/src/main/java/com/architmahes/crud/dao/EmployeeRepository.java
+++ crud/src/main/java/com/architmahes/crud/dao/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.architmahes.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 //    That's it, no need to code anything LOL
 }
