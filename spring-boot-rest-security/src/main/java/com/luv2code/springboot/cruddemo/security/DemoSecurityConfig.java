c73f7b657413dfa4849f48ac0d85100f3bf388d3
maheshwari <archit.maheshwari@payufin.com>
2023-04-18 23:48:50 +0530

    adding requestMatchers using SecurityFilterChain for role based operations

--- spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -35,4 +38,28 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, casper, archit);
 
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                configurer ->
+                        configurer
+                                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+//        Use http basic auth
+        http.httpBasic();
+
+//        Disable CSRF as not required in stateless REST API
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+
 }
