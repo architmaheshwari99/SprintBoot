a2f8210766417fc60f06c9e83a2b49896cc6d5dc
maheshwari <archit.maheshwari@payufin.com>
2023-04-18 12:23:54 +0530

    Updating entity name for custom url

--- crud/src/main/java/com/architmahes/crud/dao/EmployeeRepository.java
+++ crud/src/main/java/com/architmahes/crud/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.architmahes.crud.dao;
 
 import com.architmahes.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 //    That's it, no need to code anything LOL
 }
