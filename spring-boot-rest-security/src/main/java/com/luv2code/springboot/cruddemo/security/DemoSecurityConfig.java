673b000423f8e0d12f1660d5710e12b9a717368b
maheshwari <archit.maheshwari@payufin.com>
2023-04-19 10:26:27 +0530

    Using custom models to store user info and authority

--- spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -18,7 +18,18 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        Define query to fetch username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
