e1bf8bb38e533c94913c91e421c066243eb1e4a0
maheshwari <archit.maheshwari@payufin.com>
2023-04-20 17:12:42 +0530

    @Primary to set the primary bean

--- spring/src/main/java/com/mahes/archit/spring/HelloWorldConfiguration.java
+++ spring/src/main/java/com/mahes/archit/spring/HelloWorldConfiguration.java
@@ -2,6 +2,7 @@ package com.mahes.archit.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 record Person (String name, int age, Address address) {};
@@ -27,11 +28,13 @@ public class HelloWorldConfiguration {
 //    }
 
     @Bean(name="myaddress")
+    @Primary
     public Address address(){
         return new Address("bgmbd", "modinagar", "ind");
     }
 
     @Bean
+    @Primary
     public Person person2(){
         return new Person(name(), age(), new Address("bgmbd", "modinagar", "ind"));
     }
