9048fafdc28247520ee2bf34a94b39a9071775ef
maheshwari <archit.maheshwari@payufin.com>
2023-04-30 00:13:52 +0530

    sayHello!

---
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.mahes.archit.todowebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello!!";
+    }
+}
