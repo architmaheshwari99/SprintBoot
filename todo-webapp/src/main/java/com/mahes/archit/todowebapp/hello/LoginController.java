3989e70df6300f3347e0dc2db1c9d67ff68419f6
maheshwari <archit.maheshwari@payufin.com>
2023-04-30 15:21:33 +0530

    Login Page setup

---
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/hello/LoginController.java
@@ -0,0 +1,19 @@
+package com.mahes.archit.todowebapp.hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("login")
+    public String login() {
+        logger.debug("Logging");
+//        model.put("name", name);
+        return "login";
+    }
+}
