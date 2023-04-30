4859f1ac31b343b9e3e4bf09ba18b63ab1bea0c6
maheshwari <archit.maheshwari@payufin.com>
2023-04-30 20:41:36 +0530

    @SessionAttributes to share the model value

--- todo-webapp/src/main/java/com/mahes/archit/todowebapp/todo/TodoController.java
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/todo/TodoController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
     private TodoService todoService;
--- todo-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
+++ todo-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -5,6 +5,8 @@
     <title>Welcome Page</title>
 </head>
 <body>
+<div> Welcome ${name} </div>
+
 <div> Welcome ${todos} </div>
 
 </body>
