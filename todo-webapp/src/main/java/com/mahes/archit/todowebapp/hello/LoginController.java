b5f07c7900348396ee3dba8d74d2d7e9dff48431
maheshwari <archit.maheshwari@payufin.com>
2023-04-30 15:26:52 +0530

    Get and Post in login page and redirect to welcome page with login data

--- todo-webapp/src/main/java/com/mahes/archit/todowebapp/hello/LoginController.java
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/hello/LoginController.java
@@ -3,17 +3,27 @@ package com.mahes.archit.todowebapp.hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("login")
+    @RequestMapping(value="login", method= RequestMethod.GET)
     public String login() {
         logger.debug("Logging");
 //        model.put("name", name);
         return "login";
+    }
+
+    @RequestMapping(value="login", method= RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap map) {
+        map.put("name", name);
+        map.put("password", password);
+        return "welcome";
     }
 }
