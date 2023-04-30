236da160513741115f11fc9df971291e5a40ef07
maheshwari <archit.maheshwari@payufin.com>
2023-04-30 22:51:47 +0530

    Adding TODO controller and service logic

--- todo-webapp/src/main/java/com/mahes/archit/todowebapp/todo/TodoService.java
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/todo/TodoService.java
@@ -9,13 +9,21 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
+
+    private static int todoCount=0;
+
     static {
-        todos.add(new Todo(1, "in28mins", "Learn AWS", LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(2, "in29mins", "Learn Azure", LocalDate.now().plusYears(2), true));
-        todos.add(new Todo(3, "in30mins", "Learn GCP", LocalDate.now().plusYears(3), false));
+        todos.add(new Todo(todoCount++, "in28mins", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(todoCount++, "in29mins", "Learn Azure", LocalDate.now().plusYears(2), true));
+        todos.add(new Todo(todoCount++, "in30mins", "Learn GCP", LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean isDone){
+        Todo todo = new Todo(todoCount++ ,username, description, targetDate, isDone);
+        todos.add(todo);
     }
 }
