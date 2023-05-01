d7cd8418401c3a2763a033d9707f4abc5b025862
maheshwari <archit.maheshwari@payufin.com>
2023-05-01 18:09:09 +0530

    h2 url, allowing h2 to bypass authenticated requests by SecurityFilterChain

--- todo-webapp/src/main/java/com/mahes/archit/todowebapp/security/SpringSecurityConfiguration.java
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.mahes.archit.todowebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -42,4 +45,17 @@ public class SpringSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+          auth->auth.anyRequest().authenticated()
+        );
+//        show form login in case of unauthorized requests
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
 }
