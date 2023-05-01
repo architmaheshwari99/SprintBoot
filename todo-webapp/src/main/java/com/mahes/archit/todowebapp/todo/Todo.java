94e2863d32c2f91729ac85246d879b440dedd2ab
maheshwari <archit.maheshwari@payufin.com>
2023-05-01 18:38:58 +0530

    @Entity to make TODO a JPA entity

--- todo-webapp/src/main/java/com/mahes/archit/todowebapp/todo/Todo.java
+++ todo-webapp/src/main/java/com/mahes/archit/todowebapp/todo/Todo.java
@@ -1,12 +1,20 @@
 package com.mahes.archit.todowebapp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
 
     @Size(min=10, message = "Make the TODO more descriptive")
---
+++ todo-webapp/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'archit', 'AWS1212121212112', CURRENT_DATE(),true);
