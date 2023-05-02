a11f8fad1f9f8ed34959b85347a79b4cd1625034
maheshwari <archit.maheshwari@payufin.com>
2023-05-02 13:26:34 +0530

    add a new user,     @PostMapping("/users")

--- rest-web-app/src/main/java/com/mahes/archit/restwebapp/user/UserDAOService.java
+++ rest-web-app/src/main/java/com/mahes/archit/restwebapp/user/UserDAOService.java
@@ -12,10 +12,12 @@ public class UserDAOService {
 
     private static List<User> users = new ArrayList<>();
 
+    private static int userCount=0;
+
     static {
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eve", LocalDate.now().minusYears(35)));
-        users.add(new User(3, "Jim", LocalDate.now().minusYears(40)));
+        users.add(new User(++userCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount, "Eve", LocalDate.now().minusYears(35)));
+        users.add(new User(++userCount, "Jim", LocalDate.now().minusYears(40)));
     }
 
     public List<User> findAll() {
@@ -25,6 +27,12 @@ public class UserDAOService {
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().get();
+    }
+
+    public User save(User user) {
+        user.setId(++userCount);
+        users.add(user);
+        return user;
     }
 
 }
--- rest-web-app/src/main/java/com/mahes/archit/restwebapp/user/UserResouce.java
+++ rest-web-app/src/main/java/com/mahes/archit/restwebapp/user/UserResouce.java
@@ -1,8 +1,6 @@
 package com.mahes.archit.restwebapp.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,10 @@ public class UserResouce {
     @GetMapping("/users/{id}")
     public User retireveUser(@PathVariable int id){
         return service.findOne(id);
+    }
 
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        service.save(user);
     }
 }
