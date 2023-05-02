0a2f67aed47540cbdf9bc491935e3e2e3e28a03c
maheshwari <archit.maheshwari@payufin.com>
2023-05-02 17:38:20 +0530

    internationalization-> different user language

--- rest-web-app/src/main/java/com/mahes/archit/restwebapp/helloworld/HelloWorldController.java
+++ rest-web-app/src/main/java/com/mahes/archit/restwebapp/helloworld/HelloWorldController.java
@@ -1,14 +1,33 @@
 package com.mahes.archit.restwebapp.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("hello")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("hellov2")
+    public String helloWorldV2() {
+
+//        Get the language from Accept-Language header
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default",
+                locale);
     }
 
 
