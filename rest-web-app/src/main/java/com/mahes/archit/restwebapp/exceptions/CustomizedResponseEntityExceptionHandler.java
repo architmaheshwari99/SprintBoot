61d4c8f35f8037284e02af80b8520dd1fcd5e2f7
maheshwari <archit.maheshwari@payufin.com>
2023-05-02 16:45:27 +0530

    UserNotFoundException custom handling

--- rest-web-app/src/main/java/com/mahes/archit/restwebapp/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ rest-web-app/src/main/java/com/mahes/archit/restwebapp/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mahes.archit.restwebapp.exceptions;
 
+import com.mahes.archit.restwebapp.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,10 +15,19 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+//    Handle all exceptions
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+//    UserNotFoundException
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 }
