7af4303571933ae3e80f58490bf5d760c208e1b1
maheshwari <archit.maheshwari@payufin.com>
2023-05-02 14:03:17 +0530

    Generic Exception class to handle exceptions in a customized way

---
+++ rest-web-app/src/main/java/com/mahes/archit/restwebapp/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.mahes.archit.restwebapp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
