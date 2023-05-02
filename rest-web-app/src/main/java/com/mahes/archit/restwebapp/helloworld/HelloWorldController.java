8d5cbe313194100b37c5e69f37d32632623beec0
maheshwari <archit.maheshwari@payufin.com>
2023-05-02 12:24:49 +0530

    @PathVariable

--- rest-web-app/src/main/java/com/mahes/archit/restwebapp/helloworld/HelloWorldController.java
+++ rest-web-app/src/main/java/com/mahes/archit/restwebapp/helloworld/HelloWorldController.java
@@ -1,10 +1,7 @@
 package com.mahes.archit.restwebapp.helloworld;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -18,5 +15,10 @@ public class HelloWorldController {
     @GetMapping("hello-bean")
     public HelloBean helloBean() {
         return new HelloBean("Archit");
+    }
+
+    @GetMapping("hello-bean/{name}")
+    public HelloBean helloBean(@PathVariable String name) {
+        return new HelloBean("Archit " + name);
     }
 }
